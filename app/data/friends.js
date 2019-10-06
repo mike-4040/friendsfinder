5abfcaca4e70ee9076b350f4080a1e2b7a6ed72f
Mike Kravtsov <mike4040@me.com>
2019-10-06 09:08:29 -0700

    some test data

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    name: 'John',
+    photo: '',
+    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: 'Switcher',
+    photo: '',
+    scores: [5, 2, 3, 1, 1, 1, 5, 3, 4, 2]
+  }
+];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,9 @@
-const path = require('path');
-
 module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     res.send('GET friends');
   });
 
-  // If no matching route is found default to home
   app.post('/api/friends', function(req, res) {
-    res.sendFile('POST friend');
+    res.send('POST friend');
   });
 };
